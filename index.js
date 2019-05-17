13ea758e493e162e2f83370b38aeceb0f8636469
vinhpham0818 <vinhhvbd@gmail.com>
2019-05-17 21:32:23 +0700

    buil file ejs and nodemon

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.set('view engine', 'ejs')
+
 app.get('/api/getList', (rep, res)=> {
     res.json(['a', 'b', 'c'])
 })
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.render('index', {
+        user: {
+            name: 'pham hien vinh'
+        },
+        users: ['apple', 'samsung', 'nokia']
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
